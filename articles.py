cfc192943451682ae308f83c368c8b2683d44bde
brotatotes <hao.awesome@gmail.com>
2017-03-04 15:03:35 -0800

    add autosave feature to article extractor

--- articles.py
+++ articles.py
@@ -1,11 +1,12 @@
 from newspaper import Article
 from urllib.request import Request, urlopen, URLError
-import pickle, json, datetime, string, unicodedata
+import pickle, json, datetime, string, unicodedata, sys
 
 apikey = '&api-key='
 guardian_APIkey = apikey + 'b7275bf9-e35b-4cba-8439-b03126134973'
 nytimes_APIkey = apikey + '44c6ecf7957b48219259958b495b85a3'
 Articles =  pickle.load(open("save.pkl","rb"))
+# Articles = []
 Query = 'politics'
 
 def resetArticles():
@@ -18,7 +19,7 @@ def resetArticles():
 def guardianArticles():
 	today = datetime.date.today()
 	i = 0
-	numdays = 365*2 
+	numdays = 365
 	goalday = today - datetime.timedelta(days = numdays)
 	for i in range(numdays): # 500 days of news articles
 		t = datetime.timedelta(days = i)
@@ -26,6 +27,9 @@ def guardianArticles():
 		print("current DAY:", search_day, "; Goal:", goalday, "; DAYS Left:", numdays - i, "; Current Size:", len(Articles))
 		url = 'https://content.guardianapis.com/search?q=' + Query + '&type=article&from-date=' + search_day + '&to-date=' + search_day + guardian_APIkey
 		addGuardianArticles(url)
+		if i % 5 == 0:
+			print("AUTOSAVING!")
+			pickle.dump(Articles, open("save.pkl", "wb"))		
 	pickle.dump(Articles, open("save.pkl", "wb"))
 
 def addGuardianArticles(url):
@@ -47,20 +51,25 @@ def addGuardianArticles(url):
 	for one_article in all_ten:
 		a = Article(one_article['webUrl'])
 		a.download()
-		a.parse()
-		title = one_article['webTitle']
-		removeBar = title.find('|')
-		if removeBar > 0:
-			title = title[:removeBar]
-		title = tokenize(title)
-		text = tokenize(a.text)
-		l = len(a.text)
-		if (l >= 100) and len(title) > 0:
-			Articles.append((title, text))
-			# print("title:", title)
-			# print("length:", l, "words")
-		# else:
-		# 	print("length", l ,"article skipped.")
+
+		try:
+			a.parse()
+			title = one_article['webTitle']
+			removeBar = title.find('|')
+			if removeBar > 0:
+				title = title[:removeBar]
+			title = tokenize(title)
+			text = tokenize(a.text)
+			l = len(a.text)
+			if (l >= 100) and len(title) > 0:
+				Articles.append((title, text))
+				# print("title:", title)
+				# print("length:", l, "words")
+			# else:
+			# 	print("length", l ,"article skipped.")
+		except:
+			print("Unexpected error:", sys.exc_info()[0])
+		
 
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
