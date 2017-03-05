6dd6239272fd0bf66d66319083baee0018dbb26c
Dylan Ong <Dylan Ong>
2017-03-05 13:07:20 -0600

    made some changes to grammar rules to make it work

--- grammar_rules.py
+++ grammar_rules.py
@@ -1,34 +1,51 @@
 from linguistics import *
-from num2words import num2words
 
-
-
-HEADLINE = ""
-
-
-def delete_articles(tokenized_headline):
+def delete_articles(headline):
+	tokenized_headline = tokenize(headline)
 	articles = ["a", "an", "the", "A", "An", "The"]
 	for word in tokenized_headline:
 		if word in articles:
 			tokenized_headline.remove(word)
-	return tokenized_headline
+	return " ".join(tokenized_headline)
 
 
-def conjunction_limiter(tokenized_headline):
-	parts_of_speech = ["and", "but", "or", "for", "nor", "so", "yet", "And", "But", "Or", "For", "Nor", "So", "Yet"]
-	while tokenized_headline[0] in conjunctions:
-		tokenized_headline.remove(tokenized_headline[0])
-	while tokenized_headline[-1] in conjunctions:
-		tokenized_headline.remove(tokenized_headline[-1])
-	return tokenized_headline
+def conjunction_limiter(headline):
+	tokenized_headline = tokenize(headline)	
+	conjunctions = ["and", "but", "or", "for", "nor", "so", "yet", "And", "But", "Or", "For", "Nor", "So", "Yet"]
+	first_word = tokenized_headline[0]
 
-def preposition_limiter(tokenized_headline):
-	headline_parts_of_speech = parts_of_speech(" ".join(tokenized_headline))
-	while headline_parts_of_speech[0][0] == "IN":
-		tokenized_headline.remove(tokenized_headline[0])
-		headline_parts_of_speech.remove(headline_parts_of_speech[0][0])
-	while headline_parts_of_speech[0][-1] == "IN":
-		tokenized_headline.remove(tokenized_headline[-1])
-		headline_parts_of_speech.remove(headline_parts_of_speech[0][-1])
-	return tokenized_headline		
+	while first_word in conjunctions:
+		tokenized_headline = tokenized_headline[1:]
+		print (tokenized_headline)
+		first_word = tokenized_headline[0]
 
+	last_word = tokenized_headline[-1]
+	while last_word in conjunctions:
+		print(tokenized_headline)
+		tokenized_headline = tokenized_headline[:-1]
+		last_word = tokenized_headline[-1]
+	return " ".join(tokenized_headline)
+
+def preposition_limiter(headline):
+	tokenized_headline = tokenize(headline)	
+	headline_parts_of_speech = parts_of_speech(headline)
+	first_word = headline_parts_of_speech[0][0]
+	while first_word in ["IN","RB"]:
+		tokenized_headline = tokenized_headline[1:]
+		headline_parts_of_speech[0] = headline_parts_of_speech[0][1:]
+		first_word = headline_parts_of_speech[0][0]
+	last_word = headline_parts_of_speech[0][-1]
+	while headline_parts_of_speech[0][-1] in ["IN", "RB"]:
+		tokenized_headline = tokenized_headline[:-1]
+		headline_parts_of_speech[0] = headline_parts_of_speech[0][:-1]
+		last_word = headline_parts_of_speech[0][-1]
+	return " ".join(tokenized_headline)
+
+def delete_auxillary_verbs(headline):
+	tokenized_headline = tokenize(headline)
+	headline_parts_of_speech = parts_of_speech(headline)
+	for i in range (0, len(tokenized_headline)):
+		if headline_parts_of_speech[0][i] == "MD":
+			tokenized_headline.remove(tokenized_headline[i])
+			headline_parts_of_speech = headline_parts_of_speech[0][0:i] + headline_parts_of_speech[0][i+1:] 
+	return " ".join(tokenized_headline)
